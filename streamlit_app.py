90bdc1afb2b5d43ddcf5e1bd68a5ee169e09ee7e
rodosoliz74 <86246727+rodosoliz74@users.noreply.github.com>
2021-11-02 22:07:11 -0400

    Add files via upload
    Read and write to Firestore, securely!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,30 @@
 import streamlit as st
 from google.cloud import firestore
 
-# Authenticate to Firestore with the JSON account key.
-db = firestore.Client.from_service_account_json("firestore-key.json")
+import json
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-reddit")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("posts").document("Google")
+# Streamlit widgets to let a user create a new post
+title = st.text_input("Post title")
+url = st.text_input("Post url")
+submit = st.button("Submit new post")
 
-# Then get the data at that reference.
-doc = doc_ref.get()
+# Once the user has submitted, upload it to the database
+if title and url and submit:
+	doc_ref = db.collection("posts").document(title)
+	doc_ref.set({
+		"title": title,
+		"url": url
+	})
 
-# Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())(
+# And then render each post, using some light Markdown
+posts_ref = db.collection("posts")
+for doc in posts_ref.stream():
+	post = doc.to_dict()
+	title = post["title"]
+	url = post["url"]
+
+	st.subheader(f"Post: {title}")
+	st.write(f":link: [{url}]({url})")
