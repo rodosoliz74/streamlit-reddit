b73c77cd4b4a713943c8f940d0b7bfc5fe583500
rodosoliz74 <86246727+rodosoliz74@users.noreply.github.com>
2021-11-13 02:06:09 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ keyfile_data = json.loads(st.secrets["textkey"])
 #creds = ServiceAccountCredentials.from_json_keyfile_dict(keyfile_data)
 #db = firestore.Client(credentials=creds, project="streamlit-reddit")
 
-db = firestore.Client.from_service_account_json("firestore-key.json")
+db = firestore.Client.from_service_account_json(".streamlit/firestore-key.json")
 
 
 apptitle = 'Data Dispersion Device'
@@ -32,80 +32,158 @@ st.set_page_config(page_title=apptitle, page_icon=":eyeglasses:")
 
 st.title('Data Dispersion Device')
 
+users_ref = db.collection(u'Informacion')
+docs = users_ref.stream()
+koo=[]
+for doc in docs:
+	ko=(f'{doc.id}')
+	koo.append(ko)
+jk=len (koo)
 
+print(koo)
 
-
-# -- Page sidebar
+	
 st.sidebar.markdown("# Seleccion de Laboratorio")
-select_event = st.sidebar.selectbox('Laboratorio',['Laboratorio 1 31-10-2021','Laboratorio 2 1-11-2021', 'Laboratorio 3 2-11-2021'])
-if select_event == 'Laboratorio 1 31-10-2021':
-	coleccion='Laboratorio 1 31-10-2021'
-
-if select_event == 'Laboratorio 2 1-11-2021':
-	coleccion='Laboratorio 2 1-11-2021'	
-
-if select_event == 'Laboratorio 3 2-11-2021':
-	coleccion='Laboratorio 3 2-11-2021'	
+select_event = st.sidebar.selectbox('Laboratorio',koo)
+for l in range(jk):
+	if select_event == koo[l]:
+		coleccion=koo[l]
 	
 
 
 
 # -- Default detector list
-detectorlist = ['Laboratorio 3 2-11-2021','Laboratorio 3 2-11-2021', 'Laboratorio 3 2-11-2021']
+#detectorlist = ['Laboratorio 3 2-11-2021','Laboratorio 3 2-11-2021', 'Laboratorio 3 2-11-2021']
+
+posts_ref2  = db.collection(coleccion).where(u'tabla', u'==', "tabla1")
+docs = posts_ref2.stream()
+for doc in docs:
+	post = doc.to_dict()
+po=len(post)
+
+posts_ref3  = db.collection(coleccion).where(u'tabla', u'==', "tabla2")
+docs = posts_ref3.stream()
+for doc in docs:
+	post2 = doc.to_dict()
+po2=len(post2)
+
+posts_ref4  = db.collection(coleccion).where(u'tabla', u'==', "tabla3")
+docs = posts_ref4.stream()
+for doc in docs:
+	post3 = doc.to_dict()
+po3=len(post3)
 
 
 posts_ref  = db.collection(coleccion).where(u'tabla', u'==', "tabla1")
-
+n=1
+Adato_nn= ['44', '44', '44']
 for doc in posts_ref.stream():
-	post = doc.to_dict()
-	Adato_1 = post["dato1"]
-	Adato_2 = post["dato2"]
-	Adato_3 = post["dato3"]
-	Adato_4 = post["dato4"]
-	Adato_5 = post["dato5"]
-	Adato_6 = post["dato6"]
-	#url = post["url"]
+	for i in range(po-1):
+		post = doc.to_dict()
+		DAT=("dato"+(str(n)))
+		DOT=(''+DAT+'')
+		DUT=("Adato_"+(str(n)))
+		DUT = post[DOT]
+		#print(DUT)
 
-	#print(1dato_1[0])
-	#print(1dato_1[1])
-	#print(1dato_1[2])
-	#st.write(f":link: [{url}]({url})")
-
+		#dic3 = (Adato_nn | DUT)
+		Adato_nn=Adato_nn+DUT
+		#Adato_nn.update(DUT)
+		#print ('"'+DAT+'"')
+		n=n+1
+#print(DUT)
+Adato_nn.remove('44')
+Adato_nn.remove('44')
+Adato_nn.remove('44')
+print(Adato_nn)
 
 posts_ref  = db.collection(coleccion).where(u'tabla', u'==', "tabla2")
 
+n2=1
+Adato_nn2= ['44', '44', '44']
 for doc in posts_ref.stream():
-	post = doc.to_dict()
-	Bdato_1 = post["dato1"]
-	Bdato_2 = post["dato2"]
-	Bdato_3 = post["dato3"]
-	Bdato_4 = post["dato4"]
-	Bdato_5 = post["dato5"]
-	Bdato_6 = post["dato6"]
+	for i in range(po2-1):
+		post = doc.to_dict()
+		DAT=("dato"+(str(n2)))
+		DOT=(''+DAT+'')
+		DUT=("Bdato_"+(str(n2)))
+		DUT = post[DOT]
+		#dic3 = (Adato_nn | DUT)
+		Adato_nn2=Adato_nn2+DUT
+		#Adato_nn.update(DUT)
+		#print ('"'+DAT+'"')
+		n2=n2+1
+#print(DUT)
+Adato_nn2.remove('44')
+Adato_nn2.remove('44')
+Adato_nn2.remove('44')
 
 posts_ref  = db.collection(coleccion).where(u'tabla', u'==', "tabla3")
-
+n3=1
+Adato_nn3= ['44', '44', '44']
 for doc in posts_ref.stream():
-	post = doc.to_dict()
-	Cdato_1 = post["dato1"]
-	Cdato_2 = post["dato2"]
-	Cdato_3 = post["dato3"]
-	Cdato_4 = post["dato4"]
-	Cdato_5 = post["dato5"]
-	Cdato_6 = post["dato6"]
+	for i in range(po3-1):
+		post = doc.to_dict()
+		DAT=("dato"+(str(n3)))
+		DOT=(''+DAT+'')
+		DUT=("Cdato_"+(str(n3)))
+		DUT = post[DOT]
+		#print(DUT)
 
+		#dic3 = (Adato_nn | DUT)
+		Adato_nn3=Adato_nn3+DUT
+		#Adato_nn.update(DUT)
+		#print ('"'+DAT+'"')
+		n3=n3+1
+#print(DUT)
+Adato_nn3.remove('44')
+Adato_nn3.remove('44')
+Adato_nn3.remove('44')
 
 
 st.write(coleccion)
-st.write("Tabla 1")
-st.write(pd.DataFrame({
-    'Cordenada x': [Adato_1[0], Adato_2[0], Adato_3[0], Adato_4[0],Adato_5[0],Adato_6[0]],
-    'Cordenada y': [Adato_1[1], Adato_2[1], Adato_3[1], Adato_4[1],Adato_5[1],Adato_6[1]],
-    'Distancia de centro': [Adato_1[2], Adato_2[2], Adato_3[2], Adato_4[2],Adato_5[2],Adato_6[2]],}))
 
-df1 = pd.DataFrame({'Eje X':[Adato_1[0], Adato_2[0], Adato_3[0], Adato_4[0],Adato_5[0],Adato_6[0]],
-                   'Eje Y':[Adato_1[1], Adato_2[1], Adato_3[1], Adato_4[1],Adato_5[1],Adato_6[1]]})
-index_ = ['1', '2', '3', '4', '5','6']
+
+st.write("Tabla 1")
+a=0
+b=1
+c=2
+sep=0
+lenin=(len(Adato_nn)/3)
+lenin2=int(float(lenin))
+print(lenin)
+
+Corden1=[]
+Corden2=[]
+Corden3=[]
+conti=[]
+for j in range(lenin2):
+	print(a)
+	Cord1 = Adato_nn[a]
+	co1=float(Cord1)
+	Corden1.append(co1)
+	print(Corden1)
+	Cord2 = Adato_nn[b]
+	co2=float(Cord2)
+	Corden2.append(co2)
+	Cord3 = Adato_nn[c]
+	co3=float(Cord3)
+	Corden3.append(co3)
+	conti.append(j+1)
+	a=a+3
+	b=b+3
+	c=c+3
+	sep=sep+1
+print(conti)
+st.write(pd.DataFrame({
+    	'Cordenada x': Corden1,
+    	'Cordenada y': Corden2,
+	'Distancia de centro': Corden3,}))
+
+df1 = pd.DataFrame({'Eje X':Corden1,
+                   'Eje Y':Corden2})
+index_ = [conti]
+print(df1)
 st.vega_lite_chart(df1, {
     'mark': {'type': 'circle', 'tooltip': True},
     'encoding': {
@@ -116,18 +194,48 @@ st.vega_lite_chart(df1, {
 )
 
 
-st.write("Tabla 2")
-st.write(pd.DataFrame({
-    'Cordenada x': [Bdato_1[0], Bdato_2[0], Bdato_3[0], Bdato_4[0],Bdato_5[0],Bdato_6[0]],
-    'Cordenada y': [Bdato_1[1], Bdato_2[1], Bdato_3[1], Bdato_4[1],Bdato_5[1],Bdato_6[1]],
-    'Distancia de centro': [Bdato_1[2], Bdato_2[2], Bdato_3[2], Bdato_4[2],Bdato_5[2],Bdato_6[2]],
-})
-)
 
-df2 = pd.DataFrame({'Eje X':[Bdato_1[0], Bdato_2[0], Bdato_3[0], Bdato_4[0],Bdato_5[0],Bdato_6[0]],
-                   'Eje Y':[Bdato_1[1], Bdato_2[1], Bdato_3[1], Bdato_4[1],Bdato_5[1],Bdato_6[1]]})
-index_ = ['1', '2', '3', '4', '5','6']
-st.vega_lite_chart(df2, {
+st.write("Tabla 2")
+a=0
+b=1
+c=2
+sep=0
+lenin=(len(Adato_nn2)/3)
+lenin2=int(float(lenin))
+print(lenin)
+
+Corden1=[]
+Corden2=[]
+Corden3=[]
+conti=[]
+for j in range(lenin2):
+	print(a)
+	Cord1 = Adato_nn2[a]
+	co1=float(Cord1)
+	Corden1.append(co1)
+	print(Corden1)
+	Cord2 = Adato_nn2[b]
+	co2=float(Cord2)
+	Corden2.append(co2)
+	Cord3 = Adato_nn2[c]
+	co3=float(Cord3)
+	Corden3.append(co3)
+	conti.append(j+1)
+	a=a+3
+	b=b+3
+	c=c+3
+	sep=sep+1
+print(conti)
+st.write(pd.DataFrame({
+    	'Cordenada x': Corden1,
+    	'Cordenada y': Corden2,
+	'Distancia de centro': Corden3,}))
+
+df1 = pd.DataFrame({'Eje X':Corden1,
+                   'Eje Y':Corden2})
+index_ = [conti]
+print(df1)
+st.vega_lite_chart(df1, {
     'mark': {'type': 'circle', 'tooltip': True},
     'encoding': {
         'x': {'field': 'Eje X', 'type': 'quantitative'},
@@ -137,17 +245,46 @@ st.vega_lite_chart(df2, {
 )
 
 st.write("Tabla 3")
-st.write(pd.DataFrame({
-    'Cordenada x': [Cdato_1[0], Cdato_2[0], Cdato_3[0], Cdato_4[0],Cdato_5[0],Cdato_6[0]],
-    'Cordenada y': [Cdato_1[1], Cdato_2[1], Cdato_3[1], Cdato_4[1],Cdato_5[1],Cdato_6[1]],
-    'Distancia de centro': [Cdato_1[2], Cdato_2[2], Cdato_3[2], Cdato_4[2],Cdato_5[2],Cdato_6[2]],
-})
-)
+a=0
+b=1
+c=2
+sep=0
+lenin=(len(Adato_nn)/3)
+lenin2=int(float(lenin))
+print(lenin)
 
-df3 = pd.DataFrame({'Eje X':[Cdato_1[0], Cdato_2[0], Cdato_3[0], Cdato_4[0],Cdato_5[0],Cdato_6[0]],
-                   'Eje Y':[Cdato_1[1], Cdato_2[1], Cdato_3[1], Cdato_4[1],Cdato_5[1],Cdato_6[1]]})
-index_ = ['1', '2', '3', '4', '5','6']
-st.vega_lite_chart(df3, {
+Corden1=[]
+Corden2=[]
+Corden3=[]
+conti=[]
+for j in range(lenin2):
+	print(a)
+	Cord1 = Adato_nn3[a]
+	co1=float(Cord1)
+	Corden1.append(co1)
+	print(Corden1)
+	Cord2 = Adato_nn3[b]
+	co2=float(Cord2)
+	Corden2.append(co2)
+	Cord3 = Adato_nn3[c]
+	co3=float(Cord3)
+	Corden3.append(co3)
+	conti.append(j+1)
+	a=a+3
+	b=b+3
+	c=c+3
+	sep=sep+1
+print(conti)
+st.write(pd.DataFrame({
+    	'Cordenada x': Corden1,
+    	'Cordenada y': Corden2,
+	'Distancia de centro': Corden3,}))
+
+df1 = pd.DataFrame({'Eje X':Corden1,
+                   'Eje Y':Corden2})
+index_ = [conti]
+print(df1)
+st.vega_lite_chart(df1, {
     'mark': {'type': 'circle', 'tooltip': True},
     'encoding': {
         'x': {'field': 'Eje X', 'type': 'quantitative'},
@@ -155,7 +292,6 @@ st.vega_lite_chart(df3, {
     },
 }
 )
-
 
 
 
